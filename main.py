92fe867387d249d1b3e85a5192924de1f0b3a746
robertmakowsky <robmakowsky@icloud.com>
2024-07-16 21:05:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import time
+import os
 from PIL import Image 
 
-global im
-im = Image.open("/Users/robertmakowsky/Downloads/default.JPG")
-im.show()
 def main(Emo1,Emo2):
     dominantEmo = Emo1
     subEmo = Emo2
-    im.close()
-    im = open("/Users/robertmakowsky/Downloads/" +dominantEmo+subEmo +".png")
+    im = Image.open("/Users/robertmakowsky/Downloads/" +dominantEmo+subEmo +".png")
     im.show()
+    os.remove()
     return 1
 
 while(True):
