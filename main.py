af5900421319f5a2115c455db27936962be7f788
robertmakowsky <robmakowsky@icloud.com>
2024-07-22 13:58:39 -0600

    finally

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import os
 from PIL import Image 
+import time
+import signal
+import subprocess
+import os
 
 def main(Emo1,Emo2):
     dominantEmo = Emo1
     subEmo = Emo2
-    im = Image.open("/Users/robertmakowsky/Downloads/" +dominantEmo+subEmo +".png")
-    im.show()
-    os.remove()
+    if os.path.exists('/Users/robertmakowsky/Downloads/' +Emo1+Emo2 +'.png'):
+        print("the one piece is real")    
+    p = subprocess.Popen(["open",'/Users/robertmakowsky/Downloads/' +Emo1+Emo2 +'.png'])
+    time.sleep(10)
     return 1
-
 while(True):
-    main("fi","re")
-    main("a","s")
+    main('fi','re')
+    main('a','s')
