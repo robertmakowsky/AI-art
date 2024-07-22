629345035dff8b6c2d1ad80ff444048fe04e5748
robertmakowsky <robmakowsky@icloud.com>
2024-07-22 17:43:08 -0600

    almost done

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from PIL import Image 
+from umqtt.simple import MQTTClient
 import time
-import signal
 import subprocess
 import os
+
+CLIENT_NAME = 'mp1' #must be set to the name of the esp32 being used
+BROKER_ADDR = 'pi5ub.local'
+emotions = ['neutral', 'angry', 'disgust', 'fear', 'happy', 'sad', 'surprise'] 
+global dominantEmo 
+dominantEmo = emotions[0]
+global subEmo 
+subEmo = emotions[0]
 
 def main(Emo1,Emo2):
     dominantEmo = Emo1
     subEmo = Emo2
+    #fix the path otherwise it won't work
     if os.path.exists('/Users/robertmakowsky/Downloads/' +Emo1+Emo2 +'.png'):
-        print("the one piece is real")    
+        print("the one piece is real")
+
     p = subprocess.Popen(["open",'/Users/robertmakowsky/Downloads/' +Emo1+Emo2 +'.png'])
     time.sleep(10)
     return 1
+
 while(True):
-    main('fi','re')
-    main('a','s')
+    t = MQTTClient(CLIENT_NAME, BROKER_ADDR)
+    t.check_msg()
+    if(): 
+        dominantEmo =  MQTTClient(CLIENT_NAME, BROKER_ADDR)
+        subEmo =  MQTTClient(CLIENT_NAME, BROKER_ADDR)
+    main(dominantEmo,subEmo)
