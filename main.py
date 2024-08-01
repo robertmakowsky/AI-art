cc792b70b3e388fa1fe63081efff7f3e1a03f58f
robertmakowsky <robmakowsky@icloud.com>
2024-08-01 11:03:17 -0600

    a

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-from PIL import Image 
-from umqtt.simple import MQTTClient
 import time
-import subprocess
-import os
+import json
+import cv2
 
-CLIENT_NAME = 'mp1' #must be set to the name of the esp32 being used
+
+t = open('example_emotion_detail.json')
+data = json.load(t)
 BROKER_ADDR = 'pi5ub.local'
-emotions = ['neutral', 'angry', 'disgust', 'fear', 'happy', 'sad', 'surprise'] 
+global conversion
+emotions = {"mad","disgust", "fear","happy","sad","surprise"}
+conversion = {
+    "neutral":'Neutral',
+    "mad": 'Mad',
+    "disgust":'Disgusted',
+    "fear": 'Fear',
+    "happy":'Happy',
+    "sad":'Sad',
+    "surprise":'Surprised'
+}
 global dominantEmo 
-dominantEmo = emotions[0]
+dominantEmo = conversion['neutral']
 global subEmo 
-subEmo = emotions[0]
+subEmo = conversion['neutral']
+global folderPath 
+folderPath = '/Users/robertmakowsky/Downloads/AI Art Library/'
+
+def sub_cb(topic, msg):
+    global subEmo 
+    global dominantEmo 
+    print((topic, msg))
+    if topic == b'art/emotion':
+        dominantEmo = msg.decode('utf-8')
 
 def main(Emo1,Emo2):
-    dominantEmo = Emo1
-    subEmo = Emo2
     #fix the path otherwise it won't work
-    if os.path.exists('/Users/robertmakowsky/Downloads/' +Emo1+Emo2 +'.png'):
-        print("the one piece is real")
-
-    p = subprocess.Popen(["open",'/Users/robertmakowsky/Downloads/' +Emo1+Emo2 +'.png'])
+    image = cv2.imread(folderPath +Emo1+'-'+Emo2 +'.png')
+    #image = cv2.imread('/Users/robertmakowsky/Downloads/AI Art Library/Happy-Fear.png')
+    print(image)
+    k = cv2.waitKey(1)
+    cv2.imshow(Emo1 + Emo2,image) 
     time.sleep(10)
-    return 1
+    cv2.destroyAllWindows()
 
+
+def assign():
+    global dominantEmo
+    global subEmo
+    dominantEmo = data.emotion_details[0]
+    if data.emotion_details[0] >= 99:
+        subEmo = dominantEmo
+    else:
+        subEmo = data.emotion_details[1]
 while(True):
-    t = MQTTClient(CLIENT_NAME, BROKER_ADDR)
-    t.check_msg()
-    if(): 
-        dominantEmo =  MQTTClient(CLIENT_NAME, BROKER_ADDR)
-        subEmo =  MQTTClient(CLIENT_NAME, BROKER_ADDR)
+    #assign()
     main(dominantEmo,subEmo)
